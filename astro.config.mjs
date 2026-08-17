bce6fd4c4c101bfa9f83ff072c26110fa3bd3e2a
zzy7675 <1044187675@qq.com>
2026-08-17 18:03:42 +0800

    Deploy astro page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zzy7675.github.io',
+//   base: '/<repo-name>', // omit only if the repo is <username>.github.io
+});
